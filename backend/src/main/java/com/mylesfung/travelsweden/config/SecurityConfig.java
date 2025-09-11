b0f75e7ce964f5848a2062e8514c7bb698c96e78
Myles Fung <mylesfung@gmail.com>
2025-09-11 11:17:53 -0700

    Create SecurityConfig

--- backend/src/main/java/com/mylesfung/travelsweden/config/SecurityConfig.java
+++ backend/src/main/java/com/mylesfung/travelsweden/config/SecurityConfig.java
@@ -1,2 +1,44 @@
-package com.mylesfung.travelsweden.config;public class SecurityConfig {
+package com.mylesfung.travelsweden.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // disable in dev to allow React fetch()
+                .authorizeHttpRequests(auth -> auth
+                        // public pages do not require login
+                        .requestMatchers("/api/pages/**").permitAll()
+                        .requestMatchers("/api/services/reviews").permitAll()
+                        .requestMatchers("/api/services/create-account").permitAll()
+                        .requestMatchers("/api/services/sign-in").permitAll()
+                        // all other endpoints require secure login
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginProcessingUrl("/api/services/sign-in")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/users/logout")
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
