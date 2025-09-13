a58b9d229005013ae460abda6d880129a78a261a
Myles Fung <mylesfung@gmail.com>
2025-09-13 09:17:38 -0700

    Add lombok for Review and User models

--- backend/src/main/java/com/mylesfung/travelsweden/model/Review.java
+++ backend/src/main/java/com/mylesfung/travelsweden/model/Review.java
@@ -1,11 +1,11 @@
 package com.mylesfung.travelsweden.model;
 
 import jakarta.persistence.*;
-// Cannot use 'public record' Review data transfer object (DTO)
-// bc JPA requires no-arg constructor
+import lombok.Data;
+// Cannot use 'public record Review' DTO bc JPA requires no-arg constructor
 // and fields need to be mutable to reflect DB updates / business logic methods
 
-@Entity @Table(name = "reviews")
+@Data @Entity @Table(name = "reviews")
 public class Review {
     public Review() {}
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,16 +15,4 @@ public class Review {
     private Integer rating;
     private String description;
     private String imageURL;
-
-    public Long getId() {return id;}
-    public String getUsername() {return username;}
-    public void setUsername(String name) {this.username = name;}
-    public String getTitle() {return title;}
-    public void setTitle(String text) {this.title = text;}
-    public Integer getRating() {return this.rating;}
-    public void setRating(Integer n) {this.rating = n;}
-    public String getDescription() {return this.description;}
-    public void setDescription(String text) {this.description = text;}
-    public String getImageURL() {return this.imageURL;}
-    public void setImageURL(String path) {this.imageURL = path;}
 }
--- backend/src/main/java/com/mylesfung/travelsweden/model/User.java
+++ backend/src/main/java/com/mylesfung/travelsweden/model/User.java
@@ -1,9 +1,10 @@
 package com.mylesfung.travelsweden.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity @Table(name = "users")
-public class User {
+@Data public class User {
     public User() {}
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,17 +12,4 @@ public class User {
     private String username;
     private String password;
 
-    public Long getUid() {return this.uid;}
-    public String getUsername() {
-        return this.username;
-    }
-    public void setUsername(String name) {
-        this.username = name;
-    }
-    public String getPassword() {
-        return this.password;
-    }
-    public void setPassword(String pass) {
-        this.password = pass;
-    }
 }
